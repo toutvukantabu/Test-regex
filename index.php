ec956eaed8e3f923270e6534e733845d20f84b97
toutvukantabu <gwendal.bescont@gmail.com>
2021-07-08 13:21:54 +0200

    feat : index.php done

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+if (preg_match("#guitare|piano#i", "J'aime jouer de la guitare."))
+{
+    echo 'VRAI';
+}
+else
+{
+    echo 'FAUX';
+}
+?>
+<?php
+if (isset($_POST['telephone']))
+{
+    $_POST['telephone'] = htmlspecialchars($_POST['telephone']); // On rend inoffensives les balises HTML que le visiteur a pu entrer
+
+    if (preg_match("#^0[1-68]([-. ]?[0-9]{2}){4}$#", $_POST['telephone']))
+    {
+        echo 'Le ' . $_POST['telephone'] . ' est un numéro <strong>valide</strong> !';
+    }
+    else
+    {
+        echo 'Le ' . $_POST['telephone'] . ' n\'est pas valide, recommencez !';
+    }
+}
+?>
+</p>
+
+<form method="post">
+<p>
+    <label for="telephone">Votre téléphone ?</label> <input id="telephone" name="telephone" /><br />
+    <input type="submit" value="Vérifier le numéro" />
+</p>
+</form>
